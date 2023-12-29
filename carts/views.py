4906b33746549878f4b2450cb864e9233d118b15
komarite <ulasgulhan9@gmail.com>
2023-12-29 15:27:57 +0300

    cart item add and remove functions

--- carts/views.py
+++ carts/views.py
@@ -1,9 +1,8 @@
-from tkinter.tix import Tree
+from math import prod
 from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from store.models import Product
 from .models import Cart, CartItem
-from django.http import HttpResponse
 
 # Create your views here.
 
@@ -29,8 +28,26 @@ def add_cart(request, product_id):
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(product=product, quantity=1, cart=cart)
         cart_item.save()
-    return HttpResponse(cart_item.quantity)
-    exit()
+    return redirect('cart')
+
+
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+    return redirect('cart')
+
+
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
     return redirect('cart')
 
 
@@ -41,6 +58,8 @@ def cart(request, total=0, quantity=0, cart_item=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = total + tax
     except ObjectDoesNotExist:
         pass
 
@@ -48,6 +67,8 @@ def cart(request, total=0, quantity=0, cart_item=None):
         'total': total,
         'quantity': quantity,
         'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total,
     }
 
     return render(request, 'store/cart.html', context)
