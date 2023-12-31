4fd307a10978fc13dc64da637117cbd6864c706f
komarite <ulasgulhan9@gmail.com>
2023-12-31 20:25:43 +0300

    Search function

--- store/views.py
+++ store/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import Product
 from category.models import Category
 from carts.views import _cart_id
@@ -45,3 +47,22 @@ def product_detail(request, category_slug, product_slug):
         'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+        else:
+            return redirect('store')
+
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    
+    return render(request, 'store/store.html', context)
+
+
